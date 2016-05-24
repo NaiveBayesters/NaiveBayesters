86aa4ce7f5b4a06cb39eef3f381c43be2331a7f6
Trisha <ptaylor403@gmail.com>
2016-05-24 12:07:06 -0400

    creating initial model for classifier

--- classifier/models.py
+++ classifier/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Classifier(models.Model):
+    owner = models.ForeignKey(User)
+    name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    data = models.TextField()
