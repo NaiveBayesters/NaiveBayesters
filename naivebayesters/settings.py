3394aabbe2303ffe4a0b366dd12deef91abe2227
JonathanKross <kross.jonathan+github@gmail.com>
2016-05-24 11:09:55 -0400

    Adding classifier app to settings.py

--- naivebayesters/settings.py
+++ naivebayesters/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
+    'classifier.apps.ClassifierConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
